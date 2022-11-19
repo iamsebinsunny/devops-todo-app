26b6a423351508f8fbad44854a131b621aa4114d
sebin <sebin.sunny@sap.com>
2022-11-19 15:47:45 +0530

    update script and readme

--- bootstrap_cluster.sh
+++ bootstrap_cluster.sh
@@ -2,12 +2,12 @@
 
 #update path of cluster kubeconfig file
 export KUBECONFIG=~/.kube/gardner
+
+
+#install argocd application
+kubectl create ns argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -f apps/argocd/application/argocd.application.yaml
 
-
-#how to access argocd ui
-kubectl port-forward svc/argocd-server -n argocd 8080:443
-
-
-#how to access application via port forwarding
-kubectl port-forward -n todo pods/todo-deployment-c96b48c56-fhgpb 8081:8080 
+#install todo-app application
+kubectl apply -f apps/todo/application/todo.application.yaml
